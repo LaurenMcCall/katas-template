ed9f55eb26edf1556d6461b0e89c018a49ce1cc4
Lauren McCall <lmmccolloch@gmail.com>
2022-01-08 18:20:32 -0500

    ConvertBoolToString

---
+++ codewars/csharp/8-kyu/ConvertBoolToString.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string boolToWord(bool word)
+    {
+        if (word == true)
+        {
+            return "Yes";
+        }
+        else
+        {
+            return "No";
+        }
+    }
+}
+
+//  Complete the method that takes a boolean value and return 
+//  a "Yes" string for true, or a "No" string for false.
+
