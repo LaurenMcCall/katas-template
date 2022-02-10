e37a442e7ce92b7565f3abc57b8e01a9455687e8
Lauren McCall <lmmccolloch@gmail.com>
2022-02-10 16:58:06 -0500

    Is it even?

---
+++ codewars/csharp/8-kyu/IsItEven.cs
@@ -0,0 +1,14 @@
+public class Number
+{
+    public bool IsEven(double n)
+    {
+        if (n % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
