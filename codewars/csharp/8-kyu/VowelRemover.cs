96ab790e8a0e4a76552c2ec2f1292fa614bdf353
Lauren McCall <lmmccolloch@gmail.com>
2022-02-23 16:01:27 -0500

    Vowel Remover

--- codewars/csharp/8-kyu/VowelRemover.cs
+++ codewars/csharp/8-kyu/VowelRemover.cs
@@ -4,10 +4,6 @@ public class Kata
 {
     public static string Shortcut(string input)
     {
-        // if (input.Contains("a", "e", "i", "o", "u"))
-        // {
-
-        // }
         string vowels = "aeiou";
         var name = new string(input.Where(words => !vowels.Contains(words)).ToArray());
         return name;
